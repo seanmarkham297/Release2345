b5fc60c6a12a1ec26e288d508768d1fbc0d878e8
seanmarkham297 <sean_markham21@hotmail.com>
2021-05-21 07:43:23 +0100

    Todo model class introduced

--- app/models/Todo.java
+++ app/models/Todo.java
@@ -1,4 +1,16 @@
 package models;
 
-public class Todo {
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Todo extends Model
+{
+    public String title;
+
+    public Todo(String title)
+    {
+        this.title = title;
+    }
 }
